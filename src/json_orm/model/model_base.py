e4d53901beaf037867673b6f587c7a614a2d70f6
sadegh <sadegh.moayedizadeh@gmail.com>
2022-12-26 19:34:09 +0330

    Refactor.

---
+++ src/json_orm/model/model_base.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import Type
+
+from json_orm.query import Query
+
+
+def create_base(database_path) -> Type[ModelBase]:
+    ModelBase.query = Query(database_path)
+    return ModelBase
+
+
+class ModelBase:
+    table_name: str = None
+    query: Query
+
+    def update(self) -> None:
+        pass
+
+    def create(self) -> None:
+        self.query.create(table_name=self.table_name, **self.__dict__)
+
+    def delete(self) -> None:
+        pass
+
+    @classmethod
+    def get(cls, id: str) -> None:
+        # TODO: I want this to return a model object.
+        return cls.query.get(table_name=cls.table_name, id=id)
--- src/json_orm/query/query.py
+++ src/json_orm/query/query.py
@@ -1,22 +1,63 @@
+import json
+from contextlib import contextmanager
 from pathlib import Path
-from typing import Union, Dict, Any, List
-
-from json_orm.session import Session
+from typing import Any, Dict, Generator, List, Mapping, Union
 
 
 class Query:
-    def __init__(self, database_address: Union[Path, str]) -> None:
-        self._session = Session(database_address)
+    def __init__(self, database_path: Union[str, Path]) -> None:
+        self._database_path = database_path \
+            if isinstance(database_path, Path) else Path(database_path)
+
+    def get(self, table_name: str, id: str) -> Mapping[str, Any]:
+        with self._get_session(table_name) as table:
+            result = list(filter(lambda d: d['id'] == id, table))
+        if len(result) == 0:
+            raise ValueError('There is no entry with the given id.')
+        if len(result) > 1:
+            raise ValueError('There is more than one entry with the given id.')
+        return result.pop()
 
     def create(self, table_name: str, **kwargs) -> None:
-        with self._session.json_update(table_name) as table:
-            self._validate_against_schema(kwargs, table)
+        with self._update_session(table_name) as table:
+            self._validate_with_schema(table, kwargs)
             table.append(kwargs)
 
-    def _validate_against_schema(
-        self,
-        insert_data: Dict[str, Any],
-        table: List[Dict[str, Any]]
+    def update(self, **kwargs) -> None:
+        pass
+
+    def delete(self) -> None:
+        pass
+
+    @contextmanager
+    def _get_session(
+        self, table_name: str
+    ) -> Generator[List[Dict[str, Any]], None, None]:
+        table_path = self._database_path / (table_name + '.json')
+        json_file = open(table_path, 'r')
+        json_obj = json.load(json_file)
+        yield json_obj
+        json_file.close()
+
+    @contextmanager
+    def _update_session(
+        self, table_name: str
+    ) -> Generator[List[Dict[str, Any]], None, None]:
+        table_path = self._database_path / (table_name + '.json')
+        json_file = open(table_path, 'r+')
+        json_obj = json.load(json_file)
+        yield json_obj
+        json_file.seek(0)
+        json.dump(json_obj, json_file)
+        json_file.truncate()
+        json_file.close()
+
+    def _validate_with_schema(
+        self, table: List[Dict[str, Any]], entry: Dict[str, Any]
     ) -> None:
-        if not set(table[0].keys()) == set(insert_data.keys()):
-            raise ValueError('The insert row does not match schema.')
+        schema = self._get_schema(table)
+        if set(entry.keys()) != set(schema.keys()):
+            raise ValueError('The entry does not match the schema.')
+
+    def _get_schema(self, table: List[Dict[str, Any]]) -> None:
+        return table[0]
