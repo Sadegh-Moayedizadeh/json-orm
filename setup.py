6cec361f435bb51242dbc3973aa14a6df7df787b
sadegh <sadegh.moayedizadeh@gmail.com>
2023-01-08 01:34:16 +0330

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+	name='json_orm',
+	version='0.1.0',
+	author='Sadegh Moayedizadeh',
+	author_email='sadegh.moayedizadeh@gmail.com',
+	description='An orm for a database based on json files.',
+	long_description_content_type='text/markdown',
+	url='https://github.com/alph-mephesto/json-orm',
+	packages=setuptools.find_packages(),
+    install_requires=[],
+	license='MIT',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
